71d44aab5912de048b74de2f6d53fc677dc66527
Nicolas Zuain <nicolaszuain3@hotmail.com>
2018-01-27 07:15:58 -0300

    magia

--- Assets/NetworkAnimationController.cs
+++ Assets/NetworkAnimationController.cs
@@ -6,23 +6,27 @@ public class NetworkAnimationController : MonoBehaviour {
 
     public Animator AnimationController;
     Vector3 previousPosition = new Vector3(0, 0, 0);
+    bool m_FacingRight = true;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         AnimationController = this.GetComponent<Animator>();
 
     }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if(this.transform.position.x != previousPosition.x && Mathf.Abs(this.transform.position.y - previousPosition.y) < 0.01)
+        float moveX = this.transform.position.x - previousPosition.x;
+        float moveY = this.transform.position.y - previousPosition.y;
+
+        if (moveX != 0 && Mathf.Abs(moveY) < 0.01)
         {
             AnimationController.SetFloat("Speed", 1);
         }
         else
             AnimationController.SetFloat("Speed", 0);
 
-        if(Mathf.Abs(this.transform.position.y - previousPosition.y) > 0.01)
+        if(Mathf.Abs(moveY) > 0.01)
         {
             AnimationController.SetFloat("vSpeed", 1);
         }
@@ -31,6 +35,29 @@ public class NetworkAnimationController : MonoBehaviour {
 
         previousPosition = this.transform.position;
 
+        if (moveX > 0 && !m_FacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (moveX < 0 && m_FacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+
     }
-    
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
 }
