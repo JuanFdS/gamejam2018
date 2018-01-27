b13956eab29aa431703c5e79283b385c3a2fdd9c
Dx <joaquinibarlucia@gmail.com>
2018-01-27 01:02:49 -0300

    barra ui relleno y borde

---
+++ Assets/EnergyBarFiller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnergyBarFiller : MonoBehaviour
+{
+    public Bird birdTransform;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.localScale = new Vector3(birdTransform.energy/100, transform.localScale.y,transform.localScale.z);
+        Debug.Log(birdTransform.energy / 100);
+    }
+}
