b549a376494dbf462532ffcec2b9f1b75556f48f
Juan Manuel Fernandes dos Santos <juan.fernandes@10pines.com>
2018-01-28 05:11:41 -0300

    Por las dudas

---
+++ Assets/AntennaSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntennaSound : MonoBehaviour {
+
+	public Transform earTransform;
+	AudioSource audioSource;
+	// Use this for initialization
+	void Awake () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		var distance = Vector2.Distance(earTransform.position, transform.position);
+		audioSource.volume = 1 / distance;
+		Debug.log(distance);
+		Debug.Log(audioSource.volume);
+	}
+}
