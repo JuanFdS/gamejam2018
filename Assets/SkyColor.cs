6ec69a8bf115805ccfc7bcd96de514d86d4e7bc1
Juan Manuel Fernandes dos Santos <juan.fernandes@10pines.com>
2018-01-28 10:33:48 -0300

    Day and Night Cycle

---
+++ Assets/SkyColor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SkyColor : MonoBehaviour {
+
+	private Camera camera;
+	private Color originalBackgroundColor;
+	private Transform sun;
+	private Color originalSunColor;
+	// Use this for initialization
+	void Awake () {
+		camera = GetComponent<Camera>();
+		originalBackgroundColor = camera.backgroundColor;
+		sun = transform.GetChild(0);
+		originalSunColor = sun.GetComponent<SpriteRenderer>().color;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		var percentageOfDay = sun.GetComponent<SunMovement>().TrajectPercentage();
+		var isNight = percentageOfDay >= 0.5f;
+		if(isNight) {
+			sun.GetComponent<SpriteRenderer>().color = originalSunColor * 1.25f;
+			camera.backgroundColor = Color.black;
+		} else {
+			sun.GetComponent<SpriteRenderer>().color = originalSunColor;
+			var foo = percentageOfDay * 2;
+			var blackMagic = -5.1f * Mathf.Pow(foo - 0.5f, 2) + 0 * (foo-0.5f) + 1.2f;
+			var atardecer = (-18.3f * Mathf.Pow(foo + 0.3f, 2) + 26f * (foo + 0.3f) - 8.1f) / 3;
+			var rojo = Color.red * Math.Max(0, atardecer);
+			camera.backgroundColor = originalBackgroundColor * blackMagic + rojo;
+		}
+	}
+}
---
+++ Assets/SunMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SunMovement : MonoBehaviour {
+
+	// Use this for initialization
+	public float trajectOffset;
+	public Vector2 radius;
+	public Vector2 center;
+	public float speed;
+	public float indiceMagicoDeCircunferenciaPermitida;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		var t = (Time.time * speed) % indiceMagicoDeCircunferenciaPermitida;
+		float newX = (radius.x * Mathf.Sin(t + trajectOffset) + center.x);
+		float newY = (radius.y * Mathf.Cos(t + trajectOffset) + center.y);
+		transform.localPosition = new Vector3(newX, newY, transform.localPosition.z);
+	}
+
+	public float TrajectPercentage () {
+		return ((Time.time * speed) % (indiceMagicoDeCircunferenciaPermitida * 2)) / (indiceMagicoDeCircunferenciaPermitida * 2)	;
+	}
+}
