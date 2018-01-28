b8c1971e3d2cde0c02e76c471df293d9ff149262
Dx <joaquinibarlucia@gmail.com>
2018-01-27 23:55:24 -0300

    portada basica agregada

---
+++ Assets/TitleAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleAnimation : MonoBehaviour {
+
+    float endY;
+
+	// Use this for initialization
+	void Start () {
+        endY = transform.position.y;
+        transform.position = new Vector2(transform.position.x, transform.position.y + 150);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y > endY)
+            transform.position = new Vector2(transform.position.x, transform.position.y - 2);
+        else
+            transform.position = new Vector2(transform.position.x, endY);
+    }
+}
